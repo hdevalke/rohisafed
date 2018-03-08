fb6fb42d7bd3faafc636f32ff9fc8c4299c85e04
Hannes De Valkeneer <hannes@de-valkeneer.be>
2018-03-08 22:49:01 +0100

    test pages

---
+++ precache-manifest.075c83f83a9bca34d70e48a8ee48a477.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ff8d8e23ff4cf10e6a",
+    "url": "/rohisafed/style.css"
+  },
+  {
+    "revision": "22ff8d8e23ff4cf10e6a",
+    "url": "/rohisafed/main.bundle.js"
+  },
+  {
+    "revision": "43c4ea6bd564d68fdd30126b92196974",
+    "url": "/rohisafed/index.html"
+  }
+];
